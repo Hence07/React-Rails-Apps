aadf76ae3c1bf5bcf70acbfecf408a28ef2a778a
Prudence Codjia <pcodjia@ironguard.net>
2019-02-27 19:50:34 -0500

    React router linking component

--- app/javascript/components/EventList.js
+++ app/javascript/components/EventList.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
 class EventList extends React.Component {
-    renderEvents() {
-        const {
-            events
-        } = this.props;
-        events.sort(
-            (a, b) => new Date(b.event_date) - new Date(a.event_date),
+    renderEvents() {const {events} = this.props;
+        events.sort((a, b) => new Date(b.event_date) - new Date(a.event_date),
         );
 
         return events.map(event => ( 
-            <li key = {
-                event.id
-            } > {
-                event.event_date
-            } {
-                ' - '
-            } {
-                event.event_type
-            } 
+            <li key = {event.id} > 
+            <Link to = {`/events/${event.id}`} >
+            {event.event_date} 
+            {' - '} 
+            {event.event_type} 
+            </Link>
             </li>
         ));
     }
@@ -27,10 +21,8 @@ class EventList extends React.Component {
     render() {
         return ( 
             <section >
-            <h2 > Events < /h2> 
-            <ul > {
-                this.renderEvents()
-            } </ul> 
+            <h2 > Events </h2> 
+            <ul > {this.renderEvents()} </ul> 
             </section>
         );
     }
